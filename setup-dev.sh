d581e7628c2bc34a7b92ee7c8ad3b6f092bf7884
Levi Armstrong <levi.armstrong@gmail.com>
2016-06-06 12:42:41 -0500

    Add setup bash scripts

---
+++ setup-dev.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BASE_PATH=$HOME/qtc_plugins
+if [ "$(dirname "$PWD")" != "$BASE_PATH" ]
+then 
+    echo 'ERROR: ros_qtc_plugins must be located in: '$BASE_PATH
+    exit 1
+fi 
+
+QTC_BUILD=$BASE_PATH/qt-creator-build
+QTC_SOURCE=$BASE_PATH/qt-creator
+
+ROS_BUILD=$BASE_PATH/ros_qtc_plugins-build
+ROS_SOURCE=$BASE_PATH/ros_qtc_plugins
+
+DESKTOP_FILE=$HOME/.local/share/applications/Qt-Creator-ros.desktop
+
+# Clone Qt Creator and build it from source
+cd $BASE_PATH && git clone -b 4.0 https://github.com/qtproject/qt-creator.git
+mkdir -p $QTC_BUILD
+cd $QTC_BUILD && qmake -r $QTC_SOURCE/qtcreator.pro
+cd $QTC_BUILD && make -j8
+
+# Build ROS Qt Creator Plugin
+mkdir -p $ROS_BUILD
+cd $ROS_BUILD && qmake -r $ROS_SOURCE/ros_qtc_plugins.pro
+cd $ROS_BUILD && make -j8
+
+# Create desktop launch icon
+rm -f $DESKTOP_FILE 
+> $DESKTOP_FILE 
+echo '[Desktop Entry]' >> $DESKTOP_FILE
+echo 'Version=4.0' >> $DESKTOP_FILE
+echo 'Encoding=UTF-8' >> $DESKTOP_FILE
+echo 'Type=Application' >> $DESKTOP_FILE
+echo 'Name=QtCreator' >> $DESKTOP_FILE
+echo 'Comment=QtCreator' >> $DESKTOP_FILE
+echo 'NoDsiplay=true' >> $DESKTOP_FILE
+echo 'Exec='$QTC_BUILD/bin/qtcreator >> $DESKTOP_FILE
+echo 'Icon=QtProject-qtcreator' >> $DESKTOP_FILE
+echo 'Name[en_US]=Qt-Creator' >> $DESKTOP_FILE
+chmod +x $DESKTOP_FILE 
+
+rm -f $HOME/Desktop/QtCreator.desktop
+ln -s $DESKTOP_FILE $HOME/Desktop/QtCreator.desktop
+
+# Create user command line launch
+echo 'Add Qt Creator ROS command line launcher: /usr/local/bin/qtcreator-ros'
+sudo rm -f /usr/local/bin/qtcreator-ros 
+sudo ln -s $QTC_BUILD/bin/qtcreator /usr/local/bin/qtcreator-ros
+
