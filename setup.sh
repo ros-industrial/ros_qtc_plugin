65b869e96e0e3ceed534eea93addbf78ac1f2dd2
Levi Armstrong <levi.armstrong@gmail.com>
2016-06-23 11:48:37 -0500

    Add the ability to run setup with debug information

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ "$#" != 1 ] || ([ "$1" != "-u" ] && [ "$1" != "-d" ]); then
+if [ "$#" != 1 ] || ([ "$1" != "-u" ] && [ "$1" != "-ui" ] && [ "$1" != "-d" ] && [ "$1" != "-di" ]); then
     echo "Usage: bash setup.sh argument"
     echo "Arguments:"
-    echo "    -u : Run setup for users"
-    echo "    -d : Run setup for developers"
+    echo "    -u  : Run setup for users"
+    echo "    -d  : Run setup for developers"
+    echo "    -ui : Run setup for users with debug info"
+    echo "    -di : Run setup for developers with debug info"
     exit 85
 fi
 
@@ -11,7 +13,7 @@ fi
 echo "Install build dependencies: build-essential libgl1-mesa-dev"
 sudo apt-get install build-essential libgl1-mesa-dev
 
-if ([ "$1" == "-u" ] && [ $(basename "$PWD") != 'ros_qtc_plugin' ]) ; then
+if (([ "$1" == "-u" ] || [ "$1" == "-ui" ]) && [ $(basename "$PWD") != 'ros_qtc_plugin' ]) ; then
     BASE_PATH=$PWD/qtc_plugins
 else
     BASE_PATH=$(dirname "$PWD")
@@ -42,11 +44,15 @@ else
 fi 
 mkdir -p $QTC_BUILD
 cd $QTC_BUILD && make clean
-cd $QTC_BUILD && qmake -r $QTC_SOURCE/qtcreator.pro
+if ([ "$1" == "-u" ] || [ "$1" == "-d" ]); then
+  cd $QTC_BUILD && qmake $QTC_SOURCE/qtcreator.pro -r
+else
+  cd $QTC_BUILD && qmake $QTC_SOURCE/qtcreator.pro -r CONFIG+=qml_debug CONFIG+=force_debug_info CONFIG+=separate_debug_info
+fi
 cd $QTC_BUILD && make -j8
 
 # Build ROS Qt Creator Plugin
-if [ "$1" == "-u" ]; then
+if ([ "$1" == "-u" ] || [ "$1" == "-ui" ]); then
     if [ ! -d "$ROS_SOURCE" ]; then 
         cd $BASE_PATH && git clone --recursive -b master https://github.com/ros-industrial/ros_qtc_plugin.git
     else
@@ -58,7 +64,11 @@ cd $BASE_PATH/ros_qtc_plugin && git submodule foreach git fetch
 cd $BASE_PATH/ros_qtc_plugin && git submodule foreach git pull
 mkdir -p $ROS_BUILD
 cd $ROS_BUILD && make clean
-cd $ROS_BUILD && qmake -r $ROS_SOURCE/ros_qtc_plugin.pro
+if ([ "$1" == "-u" ] || [ "$1" == "-d" ]); then
+  cd $ROS_BUILD && qmake $ROS_SOURCE/ros_qtc_plugin.pro -r
+else
+  cd $ROS_BUILD && qmake $ROS_SOURCE/ros_qtc_plugin.pro -r CONFIG+=qml_debug CONFIG+=force_debug_info CONFIG+=separate_debug_info
+fi
 cd $ROS_BUILD && make -j8
 
 # Create desktop launch icon
