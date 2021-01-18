9b1ca6688a172186e4a92cbf8d730c9a9c545207
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-01-18 20:32:26 +0000

    trigger (fake) parsing when active target changes
    This only sets the 'm_success' flag when the active target changes. No
    actual parsing is done in the 'triggerParsing' function; this is handled in
    the old async parsing code.

--- src/project_manager/ros_build_system.cpp
+++ src/project_manager/ros_build_system.cpp
@@ -1,4 +1,5 @@
 #include "ros_build_system.h"
+#include "ros_build_configuration.h"
 
 using namespace ProjectExplorer;
 
@@ -12,7 +13,7 @@ namespace Internal {
 ROSBuildSystem::ROSBuildSystem(ROSBuildConfiguration *bc)
     : BuildSystem((BuildConfiguration*)bc)
 {
-    //
+    connect(((BuildConfiguration*)bc)->project(), &Project::activeTargetChanged, this, [this]() { triggerParsing(); });
 }
 
 void ROSBuildSystem::triggerParsing()
