7ff53cdef11d31577f529d85222d6aa44aa79de1
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-01-20 12:51:20 +0000

    implement file I/O flags in build system

--- src/project_manager/ros_build_system.cpp
+++ src/project_manager/ros_build_system.cpp
@@ -21,5 +21,49 @@ void ROSBuildSystem::triggerParsing()
     guardParsingRun().markAsSuccess();
 }
 
+bool ROSBuildSystem::addFiles(ProjectExplorer::Node *context, const QStringList &filePaths, QStringList *notAdded)
+{
+    return true;
+}
+
+ProjectExplorer::RemovedFilesFromProject ROSBuildSystem::removeFiles(ProjectExplorer::Node *context, const QStringList &filePaths, QStringList *notRemoved)
+{
+    return ProjectExplorer::RemovedFilesFromProject::Ok;
+}
+
+bool ROSBuildSystem::deleteFiles(ProjectExplorer::Node *context, const QStringList &filePaths)
+{
+    return true;
+}
+
+bool ROSBuildSystem::canRenameFile(ProjectExplorer::Node *context, const QString &filePath, const QString &newFilePath)
+{
+    return true;
+}
+
+bool ROSBuildSystem::renameFile(ProjectExplorer::Node *context, const QString &filePath, const QString &newFilePath)
+{
+    return true;
+}
+
+bool ROSBuildSystem::addDependencies(ProjectExplorer::Node *context, const QStringList &dependencies)
+{
+    return true;
+}
+
+bool ROSBuildSystem::supportsAction(ProjectExplorer::Node *context, ProjectExplorer::ProjectAction action, const ProjectExplorer::Node *node) const
+{
+    static const std::set<ProjectAction> possible_actions = {
+        ProjectAction::AddNewFile,
+        ProjectAction::AddExistingFile,
+        ProjectAction::AddExistingDirectory,
+        ProjectAction::RemoveFile,
+        ProjectAction::EraseFile,
+        ProjectAction::Rename,
+    };
+
+    return possible_actions.count(action);
+}
+
 } // namespace Internal
 } // namespace ROSProjectManager
