1d52d607913e5da7851ccbc61e615a3a999ece1f
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-01-20 12:51:20 +0000

    implement file I/O flags in build system

--- src/project_manager/ros_build_system.h
+++ src/project_manager/ros_build_system.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <projectexplorer/buildsystem.h>
+#include <projectexplorer/projectnodes.h>
 
 namespace ROSProjectManager {
 namespace Internal {
@@ -19,6 +20,15 @@ public:
     explicit ROSBuildSystem(ROSBuildConfiguration *bc);
 
     void triggerParsing() final;
+
+    virtual bool addFiles(ProjectExplorer::Node *context, const QStringList &filePaths, QStringList *notAdded = nullptr) final;
+    virtual ProjectExplorer::RemovedFilesFromProject removeFiles(ProjectExplorer::Node *context, const QStringList &filePaths,
+                                                                 QStringList *notRemoved = nullptr) final;
+    virtual bool deleteFiles(ProjectExplorer::Node *context, const QStringList &filePaths) final;
+    virtual bool canRenameFile(ProjectExplorer::Node *context, const QString &filePath, const QString &newFilePath) final;
+    virtual bool renameFile(ProjectExplorer::Node *context, const QString &filePath, const QString &newFilePath) final;
+    virtual bool addDependencies(ProjectExplorer::Node *context, const QStringList &dependencies) final;
+    virtual bool supportsAction(ProjectExplorer::Node *context, ProjectExplorer::ProjectAction action, const ProjectExplorer::Node *node) const final;
 };
 
 } // namespace Internal
