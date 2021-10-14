48349d05c4dac58ebf80983f1dcc981c2b4a4a36
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-10-15 00:04:36 +0100

    update setEnvironment to setEnvironmentChange

--- src/project_manager/ros_catkin_tools_step.cpp
+++ src/project_manager/ros_catkin_tools_step.cpp
@@ -367,7 +367,8 @@ void ROSCatkinToolsStepWidget::updateDetails()
     ROSBuildConfiguration *bc = m_makeStep->rosBuildConfiguration();
     ROSUtils::WorkspaceInfo workspaceInfo = ROSUtils::getWorkspaceInfo(bc->project()->projectDirectory(), bc->rosBuildSystem(), bc->project()->distribution());
 
-    m_ui->catkinToolsWorkingDirWidget->setEnvironment(bc->environment());
+    m_ui->catkinToolsWorkingDirWidget->setEnvironmentChange(
+                Utils::EnvironmentChange::fromFixedEnvironment(bc->environment()));
 
     ProcessParameters param;
     param.setMacroExpander(bc->macroExpander());
