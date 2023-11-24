33b52e96b70f49a92645b8ea691f5af23eb8d716
Christian Rauch <Rauch.Christian@gmx.de>
2023-11-24 23:11:03 +0100

    comment out unused variables of overloaded methods

--- src/project_manager/ros_build_system.cpp
+++ src/project_manager/ros_build_system.cpp
@@ -20,39 +20,39 @@ void ROSBuildSystem::triggerParsing()
     guardParsingRun().markAsSuccess();
 }
 
-bool ROSBuildSystem::addFiles(ProjectExplorer::Node *context, const Utils::FilePaths &filePaths, Utils::FilePaths *notAdded)
+bool ROSBuildSystem::addFiles(ProjectExplorer::Node *context, const Utils::FilePaths &/*filePaths*/, Utils::FilePaths */*notAdded*/)
 {
     // update entire workspace project
     dynamic_cast<ROSProject *>(context->getProject())->refresh();
     return true;
 }
 
-ProjectExplorer::RemovedFilesFromProject ROSBuildSystem::removeFiles(ProjectExplorer::Node *context, const Utils::FilePaths &filePaths, Utils::FilePaths *notRemoved)
+ProjectExplorer::RemovedFilesFromProject ROSBuildSystem::removeFiles(ProjectExplorer::Node */*context*/, const Utils::FilePaths &/*filePaths*/, Utils::FilePaths */*notRemoved*/)
 {
     return ProjectExplorer::RemovedFilesFromProject::Ok;
 }
 
-bool ROSBuildSystem::deleteFiles(ProjectExplorer::Node *context, const Utils::FilePaths &filePaths)
+bool ROSBuildSystem::deleteFiles(ProjectExplorer::Node */*context*/, const Utils::FilePaths &/*filePaths*/)
 {
     return true;
 }
 
-bool ROSBuildSystem::canRenameFile(ProjectExplorer::Node *context, const Utils::FilePath &oldFilePath, const Utils::FilePath &newFilePath)
+bool ROSBuildSystem::canRenameFile(ProjectExplorer::Node */*context*/, const Utils::FilePath &/*oldFilePath*/, const Utils::FilePath &/*newFilePath*/)
 {
     return true;
 }
 
-bool ROSBuildSystem::renameFile(ProjectExplorer::Node *context, const Utils::FilePath &oldFilePath, const Utils::FilePath &newFilePath)
+bool ROSBuildSystem::renameFile(ProjectExplorer::Node */*context*/, const Utils::FilePath &/*oldFilePath*/, const Utils::FilePath &/*newFilePath*/)
 {
     return true;
 }
 
-bool ROSBuildSystem::addDependencies(ProjectExplorer::Node *context, const QStringList &dependencies)
+bool ROSBuildSystem::addDependencies(ProjectExplorer::Node */*context*/, const QStringList &/*dependencies*/)
 {
     return true;
 }
 
-bool ROSBuildSystem::supportsAction(ProjectExplorer::Node *context, ProjectExplorer::ProjectAction action, const ProjectExplorer::Node *node) const
+bool ROSBuildSystem::supportsAction(ProjectExplorer::Node */*context*/, ProjectExplorer::ProjectAction action, const ProjectExplorer::Node */*node*/) const
 {
     static const std::set<ProjectAction> possible_actions = {
         ProjectAction::AddNewFile,
